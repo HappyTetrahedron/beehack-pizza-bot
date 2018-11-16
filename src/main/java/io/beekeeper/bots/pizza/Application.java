6a1bd99e9fb8e9e13b303296b754af3a11d86e59
wscsprint3r <stefan.irimescu@yahoo.com>
2018-11-16 10:23:03 +0100

    initial push

---
+++ src/main/java/io/beekeeper/bots/pizza/Application.java
@@ -0,0 +1,30 @@
+package io.beekeeper.bots.pizza;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class Application {
+    private final static String ENV_KEY_BKPR_URL = "BEEKEEPER_API_URL";
+    private final static String ENV_KEY_BKPR_KEY = "BEEKEEPER_ACCESS_KEY";
+    private final static String ENV_KEY_GIPHY_KEY = "GIPHY_API_KEY";
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        Map<String, String> env = System.getenv();
+        final String beekeeperURL = getVarOrExit(env, ENV_KEY_BKPR_URL);
+        final String beekeeperKey = getVarOrExit(env, ENV_KEY_BKPR_KEY);
+
+    }
+
+    private static String getVarOrExit(Map<String, String> env, String key) {
+        String val = env.get(key);
+        if (val == null || val.isEmpty()) {
+            throw new RuntimeException(String.format("'%s' needs to be in the environment", key));
+        } else {
+            return val;
+        }
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: stefan
+  Date: 11/16/18
+  Time: 10:08 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
