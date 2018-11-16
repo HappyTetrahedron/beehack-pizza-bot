ec9b3504ef8da80057489f0315ea4020eafe2bdf
Raffaele Canale <raffaele.canale@beekeeper.io>
2018-11-16 15:28:17 +0100

    Order pizzas in sequence

--- pizza-ordering/app.js
+++ pizza-ordering/app.js
@@ -5,6 +5,29 @@ const orders = [
     { articleId: 492, count: 1 }
 ];
 
+async function orderPizza(page, { articleId, count }) {
+    console.log('Ordering pizza', articleId);
+
+    const sizeDropdownOptionSelector = `.dropdown-menu .choose-article[data-article-id="${articleId}"]`;
+    const sizeDropdownOption = await page.$(sizeDropdownOptionSelector);
+    const productId = await page.evaluate(
+        sizeDropdown => sizeDropdown.closest("[data-article-number]").getAttribute("data-article-number")
+    , sizeDropdownOption);
+    const productElement = await page.$(`[data-article-number="${productId}"]`);
+    // console.log(productElement);
+    const dropdown = await productElement.$(".dropdown-toggle");
+    const addToCartButton = await productElement.$(".article-price-and-choose-btn :not(.choose-toppings) .choose-article");
+    console.log('Progress', articleId);
+    await dropdown.click()
+    await page.waitForSelector(sizeDropdownOptionSelector, { visible: true, timeout: 2000 });
+    await sizeDropdownOption.click();
+    await page.screenshot({path: 'b.png'});
+    await addToCartButton.click();
+    await page.screenshot({path: `${articleId}.png`});
+    console.log('Done', articleId);
+
+}
+
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
@@ -14,25 +37,12 @@ const orders = [
     await page.click('#orderPizza')
     await page.waitForSelector('.article-container')
     await page.screenshot({path: 'a.png'});
-    await Promise.all(orders.map(async({ articleId, count }) => {
-        const sizeDropdownOptionSelector = `.dropdown-menu .choose-article[data-article-id="${articleId}"]`;
-        const sizeDropdownOption = await page.$(sizeDropdownOptionSelector);
-        const productId = await page.evaluate(
-            sizeDropdown => sizeDropdown.closest("[data-article-number]").getAttribute("data-article-number")
-        , sizeDropdownOption);
-        const productElement = await page.$(`[data-article-number="${productId}"]`);
-        // console.log(productElement);
-        const dropdown = await productElement.$(".dropdown-toggle");
-        const addToCartButton = await productElement.$(".article-price-and-choose-btn :not(.choose-toppings) .choose-article");
 
-        await dropdown.click()
-        await page.waitForSelector(sizeDropdownOptionSelector, { visible: true, timeout: 2000 });
-        await sizeDropdownOption.click();
-        await page.screenshot({path: 'b.png'});
-        await addToCartButton.click();
-        await page.screenshot({path: `${articleId}.png`});
+    return orders.reduce((promiseChain, order) => {
+        return promiseChain.then(() => orderPizza(page, order));
+    }, Promise.resolve());
 
-    }));
+    // await Promise.all(orders.map());
 
-    await browser.close();
+    // await browser.close();
 })();
