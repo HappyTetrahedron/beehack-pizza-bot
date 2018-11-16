b1cbe5df99ffe4956aa72d1cdb304b29fffd2241
Roland Meyer <roland.meyer@beekeeper.io>
2018-11-16 10:52:20 +0100

    Bootstrapped chatbot

--- src/main/java/io/beekeeper/bots/pizza/Application.java
+++ src/main/java/io/beekeeper/bots/pizza/Application.java
@@ -6,24 +6,14 @@ import org.slf4j.LoggerFactory;
 import java.util.Map;
 
 public class Application {
-    private final static String ENV_KEY_BKPR_URL = "BEEKEEPER_API_URL";
-    private final static String ENV_KEY_BKPR_KEY = "BEEKEEPER_ACCESS_KEY";
+    private final static String BASE_URL = "https://pizza.dev.beekeeper.io";
+    private final static String API_TOKEN = "2c39a8b4-4af7-44df-abe0-236d9de0ab27";
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        Map<String, String> env = System.getenv();
-        final String beekeeperURL = getVarOrExit(env, ENV_KEY_BKPR_URL);
-        final String beekeeperKey = getVarOrExit(env, ENV_KEY_BKPR_KEY);
-
+        PizzaBot bot = new PizzaBot(BASE_URL, API_TOKEN);
+        bot.start();
     }
 
-    private static String getVarOrExit(Map<String, String> env, String key) {
-        String val = env.get(key);
-        if (val == null || val.isEmpty()) {
-            throw new RuntimeException(String.format("'%s' needs to be in the environment", key));
-        } else {
-            return val;
-        }
-    }
 }
